7ca80443358c25acda1d1cb3f317f82e9a38225e
Shruti Gavali <gavalishruti21@gmail.com>
2022-07-06 01:38:02 +0530

    Day 40

---
+++ Day_040/count-no-of--leaf-nodes.cpp
@@ -0,0 +1,36 @@
+/**********************************************************
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+      public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+        }
+    };
+***********************************************************/
+void inOrder(BinaryTreeNode<int> *root, int &count)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    inOrder(root->left, count);
+    if(root->left == NULL && root->right == NULL)
+    {
+        count++;
+    }
+    inOrder(root->right, count);
+}
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    int count =0;
+    // Traversing tree using inorder traversal
+    inOrder(root, count);
+    return count;
+}
