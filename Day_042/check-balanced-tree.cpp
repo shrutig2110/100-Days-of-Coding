666b21e022957625ed0b191e89cc0422e92ba050
Shruti Gavali <gavalishruti21@gmail.com>
2022-07-07 19:57:18 +0530

    Day 42

--- Day_042/check-balanced-tree.cpp
+++ Day_042/check-balanced-tree.cpp
@@ -7,14 +7,17 @@ pair<bool,int> balance(Node* root)
         pair<bool,int> p = make_pair(true,0);
         return p;
     }
-    
+// Check if left root is balanced or not
     pair<bool,int> left = balance(root->left);
+// Check if right root is balanced or not
     pair<bool,int> right = balance(root->right);
-    
+
     int leftans = left.first;
     int rightans = right.first;
     bool diff = abs(left.second - right.second) <=1 ; 
     pair<bool,int> ans;
+// For a tree to be balanced it's left and right subtree must be balanced(true) 
+// and difference of height of left and right subtree must be <= 1
     if(leftans && rightans && diff)
     {
         ans.first = true;
@@ -22,6 +25,7 @@ pair<bool,int> balance(Node* root)
     else{
         ans.first = false;
     }
+// Calculate the height of current node to be able to find difference later 
     ans.second = max(left.second, right.second) +1;
     return ans;
 }
---
+++ Day_042/identical-trees-or-not.cpp
@@ -0,0 +1,26 @@
+   
+class Solution
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        if(r1 == NULL && r2==NULL)
+        {
+            return true;
+        }
+        if(r1 == NULL && r2!=NULL || r1!= NULL && r2==NULL )
+        {
+            return false;
+        }
+        int left = isIdentical(r1->left,r2->left);
+        int right = isIdentical(r1->right,r2->right);
+        if(left && right && r1->data == r2->data)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
