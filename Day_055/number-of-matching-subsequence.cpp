146f2d0d8d3cd70df026644f4946a15675183c78
Shruti Gavali <gavalishruti21@gmail.com>
2022-07-20 22:34:05 +0530

    Day 55

---
+++ Day_055/number-of-matching-subsequence.cpp
@@ -0,0 +1,24 @@
+/*
+PROBLEM: Number of Matching Subsequences
+*/ 
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int ans = 0;
+        vector<vector<int>> mappings(26);  // stores array of indices for each character in s
+        for(int i = 0; i < size(s); i++) mappings[s[i] - 'a'].push_back(i);
+        for(auto& word : words) {
+            bool found = true;
+            // i = index in word | prev = index in s matched for word[i-1]
+            for(int i = 0, prev = -1; found && i < size(word); i++) {
+                auto& v = mappings[word[i]-'a'];
+                // check if current character exists in s with index > prev
+                auto it = upper_bound(begin(v), end(v), prev);   
+                if(it == end(v)) found = false;                  // doesn't exist
+                else prev = *it;                                 // update prev for next check
+            }
+            ans += found;
+        }
+        return ans;
+    }
+};
