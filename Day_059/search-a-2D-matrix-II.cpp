0bc535eda692ef6d9bd9863db9cf74a604cfe23a
Shruti Gavali <gavalishruti21@gmail.com>
2022-07-24 23:50:44 +0530

    Day 59

---
+++ Day_059/search-a-2D-matrix-II.cpp
@@ -0,0 +1,27 @@
+/*
+PROBLEM: Search a 2D Matrix II
+*/ 
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int i=0,j=col-1;
+        while(i<row & j>=0)      
+        {
+            if(matrix[i][j]==target)
+            {
+                return 1;
+            }
+            else if(matrix[i][j]<target)
+            {
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return 0;
+    }
+};
