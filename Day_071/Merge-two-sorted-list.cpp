4733008a1057b73164b4087ea751cc191aceb67a
Shruti Gavali <gavalishruti21@gmail.com>
2022-08-06 23:11:56 +0530

    Day 71

---
+++ Day_071/Merge-two-sorted-list.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
+    {
+        if(!list1) return list2;
+        if(!list2) return list1;
+        
+        ListNode* head =  list1;
+        if(list1->val < list2->val)
+        {
+            list1 = list1->next;
+        }
+        else{
+            head = list2;
+            list2 = list2->next;
+        }
+        ListNode* curr =  head;
+        while(list1 && list2)
+        {
+             if(list1->val < list2->val)
+            {
+                 curr->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+        if(list1) curr->next = list1;
+        else curr->next = list2;
+        return head;
+    }
+};
