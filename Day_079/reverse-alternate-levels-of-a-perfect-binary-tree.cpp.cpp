8a6b298e78292771f957065229fee9657eec5bbc
Shruti Gavali <gavalishruti21@gmail.com>
2022-08-14 17:03:14 +0530

    Day 79

---
+++ Day_079/reverse-alternate-levels-of-a-perfect-binary-tree.cpp.cpp
@@ -0,0 +1,24 @@
+/*
+PROBLEM: Reverse Alternating Levels of a Perfect Binary Tree
+*/
+class Solution {
+public:
+    int findMaxPathSum(TreeNode* root, int &max_sum)
+    {
+      if (root == NULL) return 0;
+//    To avoid null or negative values so comparing with 0
+      int leftMaxPath = max(0, findMaxPathSum(root -> left, max_sum));
+      int rightMaxPath = max(0, findMaxPathSum(root -> right, max_sum));
+      int val = root -> val;
+//    For storing max sum of any subtree
+      max_sum = max(max_sum, (leftMaxPath + rightMaxPath) + val);
+//    return the maximum of sub-tree
+      return max(leftMaxPath, rightMaxPath) + val;
+
+    }
+    int maxPathSum(TreeNode* root) {
+        int max_sum = INT_MIN;
+        findMaxPathSum(root, max_sum);
+        return max_sum;
+    }
+};
